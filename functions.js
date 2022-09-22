e338dda8703dfc646d35a0172cef1a5f50dbc788
Keenan Southall <keenan.southall@yahoo.com>
2022-09-22 12:55:46 -0600

    Add code and documentation

--- functions.js
+++ functions.js
@@ -1,13 +1,44 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
 
+function printGreeting() {
+  console.log('hi!');
+}
+
+printGreeting();
+printGreeting();
+printGreeting();
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 
+function greetPerson(name) {
+  console.log(`Hi ${name}`);
+}
+
+greetPerson("Steve");
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 
+function companyPayRange(companyName, minPay, maxPay) {
+  console.log(`Here at ${companyName} our minimum pay is $${minPay} and our maximum pay is $${maxPay}`);
+  var payRange = maxPay - minPay;
+  console.log(`That would make our pay range $${payRange}`);
+}
+
+companyPayRange("Exezelecommunicationsnetwork", 7, 11);
 
 // 4: Write a function that satifies the following interaction pattern:
+
+function checkStock(qty, item) {
+  if (qty === 0) {
+    console.log(`${item} - OUT of stock!`);
+  }
+  else if (qty >= 4) {
+    console.log(`${item} is stocked`);
+  }
+  else {
+    console.log(`${item} - running LOW`);
+  }
+}
 
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
--- selfEval.md
+++ selfEval.md
@@ -1,6 +1,19 @@
 # Self Evaluation
 
 - Reflect on your time management and awareness of where you spend time. Is this a strength or area you are working on? What support do you need in this area? And/or, what tips do you have to share with your classmates in this area?
+
+time management is getting better everyday. Personally i like trello more than google calendar. Trello makes it much easier to get specific tasks done one at a time and stay on schedule. try trello :)
+
 - How would you explain to a 5 year old what a function is?
+
+a function is like a recipe, you can write whatever you want in the recipe. But, the recipe will never
+be made until you actually make it (call it)!
+
 - Share your understanding of arguments and parameters.
-- What questions/topics (related to Functions) do you want to being to your Small Group?
+
+the parameters are the hard coded variables in the Function
+the arguments are the actual data you pass in when the function is called
+
+- What questions/topics (related to Functions) do you want to bring to your Small Group?
+
+how can we collaborate more and better together to help and accelarate everyones learning?
